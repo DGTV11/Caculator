4c92d5b4cd29a9b61ed42879f4a334cf8d4692b1
DGTV11 <52272655+DGTV11@users.noreply.github.com>
2020-03-26 16:12:39 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Caculator',
+      version='0.1',
+      description='You can use this to calculate problems you can do by hand',
+      url='http://github.com/DGTV11/funniest',
+      author='DGTV11',
+      author_email='danielweedk@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
